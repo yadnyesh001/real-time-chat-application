7a148235147665070e243bef60830104bae7ad03
Yadnyesh001 <yadnyeshbadgujar2004@gmail.com>
2025-08-16 01:09:01 +0530

    small changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,10 +5,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
 	plugins: [react()],
 	server: {
-		port: 5000,
+		port: 3000,
 		proxy: {
 			"/api": {
-				target: "http://127.0.0.1:3000",
+				target: "http://localhost:5000",
 			},
 		},
 	},
